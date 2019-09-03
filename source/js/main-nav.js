b038eef947aa3051b265ba9da341b14bba0838ca
Artem Kabanin <akabanin6@gmail.com>
2019-09-03 22:03:35 +0300

    Add image for map when no js

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,8 +1,10 @@
 var header = document.querySelector('.header');
+var map = document.querySelector('.map');
 var navMain = document.querySelector('.main-nav');
 var toggle = document.querySelector('.header__toggle');
 
 navMain.classList.remove('main-nav--nojs');
+map.classList.remove('map--nojs');
 
 toggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
